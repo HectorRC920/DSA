845a97cc9311129e7663ac93893861e521e4df3c
hrubio <hectorrubio@grupopromessa.com>
2023-10-13 08:11:04 -0600

    Linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -117,6 +117,24 @@ class LinkedList {
     oneBefore.next = toRemove.next
     toRemove.next = null
   }
+  reverse(){
+    let temp = this.head
+    this.head = this.tail
+    this.tail = temp
+
+    let next = temp.next
+    let prev = null
+    for (let i = 0; i < this.length; i++) {
+      
+      next = temp.next
+      temp.next = prev
+      prev = temp
+      temp = next
+      
+      
+    }
+    return this
+  }
 }
 
 class Node {
